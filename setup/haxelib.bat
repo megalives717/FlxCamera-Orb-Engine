a9f8b38f2c7d5474f81cbfed521378737b7d248e
megalives717 <181967496+megalives717@users.noreply.github.com>
2025-02-13 22:21:04 +0300

    Create haxelib.bat

---
+++ setup/haxelib.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+ECHO Install haxe
+start "" https://haxe.org/download
+pause
+@ECHO OFF
+ECHO Install all libraries
+haxelib install flixel 5.9.0
+haxelib install flixel-addons 3.3.0
+haxelib install flixel-ui 2.6.3
+haxelib install nape-haxe4 2.0.22
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate
+@ECHO OFF
+ECHO setup complete
+pause
